ee0102d762b52005b38ea1efff20d8753b5f43bc
miguel angel riveros mejia <riverosmejiamiguel@gmail.com>
2024-08-25 22:32:53 -0500

    slider funcional

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,39 @@ document.getElementById("menu-icon1").addEventListener("click", function() {
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+
+    const slides = document.querySelector('.slides'); //buscar todas las tarjetas y meterlas a un arreglo
+    console.log(slides);
+
+    //-----//
+    
+    const slideCount = document.querySelectorAll('.slide').length;
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+
+    //----->selecciona todos los elementos que vamos a usar (el espacio de las tarjetas, botones de izq. y der.)
+
+    let currentIndex = 0; //índice
+
+    function updateSlidePosition() {
+        // Calcula el desplazamiento del slider y aplica la transformación
+        slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    prevButton.addEventListener('click', function() {
+        // Mueve al slide anterior si no es el primero
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateSlidePosition();
+        }
+    });
+
+    nextButton.addEventListener('click', function() {
+        // Mueve al siguiente slide si no es el último
+        if (currentIndex < slideCount - 1) {
+            currentIndex++;
+            updateSlidePosition();
+        }
+    });
+});
